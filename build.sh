f0d57c520630dddd026540418c69e3bab562c66c
brunoton <brunoton@gmail.com>
2024-10-12 19:27:12 -0300

    small layout improvements

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Running ConclusionsBuilder.py..."
+python3 ConclusionsBuilder.py
+
+echo "Running ResultBuilder.py..."
+python3 ResultBuilder.py
+
+echo "Both scripts have been executed."
